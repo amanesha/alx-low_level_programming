a248bccfadc28e032231528961b8f56671b28520
amanesha <amaneyasu123@gmail.com>
2023-02-16 23:57:37 +0300

    4inputs

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,1 +1,10 @@
-#!/bin/bash
+#include <stdio.h>
+/**
+ * main - Entry poing
+ * Return: Always 0 (Success)
+ */
+int main (void)
+{
+	printf("with proper grammer, but the outcome is a piece of art, \n");
+	return (0);
+}
