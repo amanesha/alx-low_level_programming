1a220335f01ff8d1b92b25c55e8a6d9467010e9f
amanesha <amaneyasu123@gmail.com>
2023-02-21 15:54:17 +0300

    func3

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,10 +6,10 @@
 void print_alphabet_x10(void)
 {
 char c;
-int i= 0;
-while (i < = 9)
+int i = 0;
+while (i <= 9)
 {
-for (c = 'a'; c < = 'z'; c++)
+for (c = 'a'; c <= 'z'; c++)
 {
 _putchar (c);
 }
