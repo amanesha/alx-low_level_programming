2e08531535755774bee946c1ca3eb0b47627d8ba
amanesha <amaneyasu123@gmail.com>
2023-02-28 12:41:02 +0300

    5-rev_String.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -10,13 +10,14 @@ char rev = s[0];
 int counter = 0;
 int i;
 while (s[counter] != '\0')
-counter ++ ;
-for (i = 0; i < counter; i++)
+counter++ ;
+for (i = 0 ; i < counter ; i++)
 {	
-counter -- ;
+counter-- ;
 rev = s[i];
 s[i] = s[counter];
 s[counter] = rev;
 }
 }
+
 
